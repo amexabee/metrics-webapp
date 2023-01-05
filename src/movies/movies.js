2b2c2ad2cf95cf87b431022e810de6fbbb040222
amexabee <amexabee@yahoo.com>
2023-01-05 13:27:06 +0200

    Implement search functionality

--- src/movies/movies.js
+++ src/movies/movies.js
@@ -1,18 +1,30 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { useSelector } from 'react-redux';
 import Poster from './poster';
 import './movies.css';
+import SearchInput from '../components/search';
 
 const Movies = () => {
-  const moviesList = useSelector((state) => state.movies);
+  const list = useSelector((state) => state.movies);
+  const [search, setSearch] = useState('');
+
+  const handleChange = (e) => {
+    setSearch(e.target.value);
+  };
+
+  const filtered = list.filter((movie) => movie.name.toLowerCase().includes(search.toLowerCase()));
+
   return (
-    <div className="grid-container">
-      {moviesList.map((movie) => (
-        <div className="grid-item" key={movie.id}>
-          <Poster id={movie.id} name={movie.name} images={movie.images} />
-        </div>
-      ))}
-    </div>
+    <>
+      <SearchInput search={search} handleChange={handleChange} />
+      <div className="grid-container">
+        {filtered.map((movie) => (
+          <div className="grid-item" key={movie.id}>
+            <Poster id={movie.id} name={movie.name} images={movie.images} />
+          </div>
+        ))}
+      </div>
+    </>
   );
 };
 
